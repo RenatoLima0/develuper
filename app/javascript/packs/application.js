479203abc771e41268ebb3f63c5207e570cf4ea3
Matheus Moreira Ferreira <moreira.mferreira@gmail.com>
2020-06-09 13:51:31 -0300

    geocoder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,7 +21,11 @@ require("channels")
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
+import { initMapbox } from '../plugins/init_mapbox';
 
+document.addEventListener('turbolinks:load', () => {
+  initMapbox();
+})
 // External imports
 import "bootstrap";
 
