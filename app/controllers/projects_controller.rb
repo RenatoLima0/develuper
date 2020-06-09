d0c0fed0806c7301fc7b9acbd564c42db5509baf
andre <aguiar1001@hotmail.com>
2020-06-09 10:58:39 -0300

    t

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,8 +16,8 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
-    project_owner = ProjectOwner.find(current_user.id)
-    @project.project_owner = project_owner
+    project_owner = ProjectOwner.create(user_id: current_user.id)
+     @project.project_owner = project_owner
     if @project.save
       redirect_to @project, notice: 'Create Sucessfully'
     else
