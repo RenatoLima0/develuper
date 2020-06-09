1b6e320f943e4bdb79f96f022c996d5c52f778fd
andre <aguiar1001@hotmail.com>
2020-06-09 17:03:02 -0300

    versão role

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'pages#home'
+  
+  resources :dashboards, only: [:index]
   resources :projects, only: [:index, :show, :new, :create, :edit, :destroy]
+  resources :profiles, only: [:new, :create]
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
