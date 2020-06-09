af11a83cd0f5bd82d6b6ad908af44fc26cb4fe6e
andre <aguiar1001@hotmail.com>
2020-06-09 15:57:05 -0300

    t

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :address, :phone, :presence: true, on: :update
 end
