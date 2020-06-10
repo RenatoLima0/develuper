d4610ef057e9ffd4f5166f479bb84d69c53376a4
andre <aguiar1001@hotmail.com>
2020-06-10 15:44:54 -0300

    fix the bug map, after this fix function for check both users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,18 @@ class User < ApplicationRecord
   # after_create :create_role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  geocoded_by :address
 
+  after_validation :geocode, if: :will_save_change_to_address?
+  
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
-
-
-validates :name, :address, :phone, presence: true, on: :update
-
+  
+  
+  validates :name, :address, :phone, presence: true, on: :update
+  
+    
+  
 # def create_role
 #     if current_user.role == Developer
 #       root_path
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_143932) do
+ActiveRecord::Schema.define(version: 2020_06_10_173618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2020_06_09_143932) do
     t.integer "phone"
     t.string "name"
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
