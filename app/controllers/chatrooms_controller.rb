1b7af17daab56a5e8477b671ae2f4247757808b6
Matheus Moreira Ferreira <moreira.mferreira@gmail.com>
2020-06-11 11:13:01 -0300

    conlito again

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,4 +2,22 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
   end
+
+  def new
+    @chatroom = Chatroom.new
+  end
+
+    def create
+      @chatroom = Chatroom.new(project_params)
+      project_owner = ProjectOwner.create(user_id: current_user.id)
+  
+      @chatroom.project_owner = project_owner
+      if @project.save
+        redirect_to @chatroom, notice: 'Create Sucessfully'
+      else
+        render 'new'
+      end
+    end
+
+  
 end
