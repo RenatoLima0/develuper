c693e905e625c904503f8c8ef55fcb6a659f08d4
Matheus Moreira Ferreira <moreira.mferreira@gmail.com>
2020-06-11 18:59:24 -0300

    chatroom index ok

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,9 @@
 class ChatroomsController < ApplicationController
 
-
+  def index 
+   @chatrooms = Chatroom.all
+  end
+  
   def show
     @message = Message.new
     @chatroom = Chatroom.find(params[:id])
