16dc30ac0c93e6f0cc53c93db5524c12bd855562
andre <aguiar1001@hotmail.com>
2020-06-15 13:42:10 -0300

    adicionando link to do dash

--- db/schema.rb
+++ db/schema.rb
@@ -93,7 +93,7 @@ ActiveRecord::Schema.define(version: 2020_06_15_133938) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "role"
-    t.integer "phone"
+    t.string "phone"
     t.string "name"
     t.string "address"
     t.float "latitude"
