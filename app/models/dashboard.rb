f94f6f0758950560a4fbd543195f1f6e83da4d29
Renato Lima <renatoslima46@gmail.com>
2020-06-17 10:28:56 -0300

    vamos la de novo

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,27 +1,26 @@
 Rails.application.config.active_record.belongs_to_required_by_default = false
 
 class Dashboard < ApplicationRecord
-    # Search(order matters ? )
-    include PgSearch::Model
-    pg_search_scope :search_by_title_and_description,
-    against: [:title, :description],
-    using: {
-        tsearch: {
-            prefix: true
-        }
-    }
+	# Search(order matters ? )
+	include PgSearch::Model
+	pg_search_scope :search_by_title_and_description,
+	against: [:title, :description],
+	using: {
+		tsearch: {
+			prefix: true
+		}
+	}
 
-    geocoded_by :address
-    after_validation :geocode, if :will_save_change_to_address
-    has_many :chatrooms
+	geocoded_by :address
+	after_validation :geocode, if: :will_save_change_to_address?
+	has_many :chatrooms
 
-    belongs_to :project_owner
+	belongs_to :project_owner
 
-    validates :title, presence: true
-    validates :description, presence: true
+	validates :title, presence: true
+	validates :description, presence: true
 
-    def project_owner_address
-        project_owner.user.address
-    end
-
+	def project_owner_address
+			project_owner.user.address
+	end
 end
