6e0159054956fe5638639f180af20e5cca6736ef
andre <aguiar1001@hotmail.com>
2020-06-17 15:39:24 -0300

    fix portifolio

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :profiles, only: [:new, :create]
   resources :chatrooms, only: [:show, :index]
   resources :developers, only: [:index, :show, :new, :create, :edit, :destroy]
-  resources :portifolios, only: [:index, :new, :create, :edit, :destroy]
+  resources :portifolios, only: [:index, :new, :create, :edit, :destroy, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
