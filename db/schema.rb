497bf6c524e733a05b1711774019caa3521c7cde
Renato Lima <renatoslima46@gmail.com>
2020-06-17 14:20:30 -0300

    title to portifolio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_134011) do
+ActiveRecord::Schema.define(version: 2020_06_17_134020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_06_17_134011) do
     t.bigint "developer_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
     t.index ["developer_id"], name: "index_portifolios_on_developer_id"
   end
 
